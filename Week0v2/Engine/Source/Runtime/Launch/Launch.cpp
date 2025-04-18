07215e712c71e20de3598d8e95747148565abdce
wj <cjs1744@gmail.com>
2025-04-18 17:37:19 +0900

    Console창 띄우기 std::cout 사용가능

--- Week0v2/Engine/Source/Runtime/Launch/Launch.cpp
+++ Week0v2/Engine/Source/Runtime/Launch/Launch.cpp
@@ -5,8 +5,18 @@
 
 FEngineLoop GEngineLoop; 
 
+void RedirectIOToConsole() {
+    AllocConsole();
+    FILE* fp; freopen_s(&fp, "CONOUT$", "w", stdout); freopen_s(&fp, "CONOUT$", "w", stderr); freopen_s(&fp, "CONIN$", "r", stdin); std::cout << "Debug Console Initialized!" << std::endl;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+
+#ifdef _DEBUG 
+    RedirectIOToConsole();
+#endif
+
     // 사용 안하는 파라미터들
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
@@ -17,4 +27,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     GEngineLoop.Exit();
 
     return 0;
+
+#ifdef _DEBUG
+    FreeConsole();
+#endif
 }
+
