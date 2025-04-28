81dd06964b690fc5944f3270ad2372924af2555e
kbjun98 <110660221+kbjun98@users.noreply.github.com>
2025-04-29 05:26:31 +0900

    마우스 커서 중앙 고정, AABB Show Flag로 전부 끄기

--- Week0v2/Engine/Source/Contents/AGamePlayer.cpp
+++ Week0v2/Engine/Source/Contents/AGamePlayer.cpp
@@ -17,11 +17,19 @@ AGamePlayer::AGamePlayer(const AGamePlayer& Other) : Super(Other)
 void AGamePlayer::BeginPlay()
 {
     Super::BeginPlay();
-    ShowCursor(FALSE);
-    bShowCursor = false;
+
+    CURSORINFO cursorInfo = { sizeof(CURSORINFO) };
+    GetCursorInfo(&cursorInfo);
+    if (cursorInfo.flags == CURSOR_SHOWING)
+    {
+        ShowCursor(FALSE);
+        bShowCursor = false;
+    }
+
     GetCursorPos(&lastMousePos);
     UCameraComponent* Camera = GetComponentByClass<UCameraComponent>();
     GEngine->GetLevelEditor()->GetActiveViewportClient()->SetOverrideComponent(Camera);
+    UE_LOG(LogLevel::Display, "AGamePlayer Begin Play");
 }
 
 void AGamePlayer::Tick(float DeltaTime)
@@ -33,9 +41,15 @@ void AGamePlayer::Tick(float DeltaTime)
 void AGamePlayer::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
     Super::EndPlay(EndPlayReason);
-    ShowCursor(TRUE);
-    bShowCursor = true;
+    CURSORINFO cursorInfo = { sizeof(CURSORINFO) };
+    GetCursorInfo(&cursorInfo);
+    if (cursorInfo.flags != CURSOR_SHOWING)
+    {
+        ShowCursor(TRUE);
+        bShowCursor = true;
+    }
     GEngine->GetLevelEditor()->GetActiveViewportClient()->SetOverrideComponent(nullptr);
+    UE_LOG(LogLevel::Display, "AGamePlayer End Play");
 }
 
 UObject* AGamePlayer::Duplicate() const
@@ -104,6 +118,9 @@ void AGamePlayer::Input(float DeltaTime)
         POINT currentMousePos;
         GetCursorPos(&currentMousePos);
 
+
+
+
         int32 deltaX = currentMousePos.x - lastMousePos.x;
         int32 deltaY = currentMousePos.y - lastMousePos.y;
 
@@ -117,8 +134,15 @@ void AGamePlayer::Input(float DeltaTime)
 
         SetActorRotation(Rotation);
 
+        RECT rect;
+        GetClientRect(GetActiveWindow(), &rect);
+        POINT center;
+        center.x = (rect.right - rect.left) / 2;
+        center.y = (rect.bottom - rect.top) / 2;
+        ClientToScreen(GetActiveWindow(), &center);
+        SetCursorPos(center.x, center.y);
         // 다음 프레임을 위해 현재 마우스 위치 저장
-        lastMousePos = currentMousePos;
+        lastMousePos = center;
     }
 
     FVector MoveDirection = FVector::ZeroVector;
