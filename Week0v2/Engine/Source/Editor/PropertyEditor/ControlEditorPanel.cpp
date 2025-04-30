25d8eda54b87fd0ad9e3cffc9d8f586153a5e0a3
nyeong <idpasudwns@gmail.com>
2025-05-01 04:57:20 +0900

    chore: blur 시각화 더 많이

--- Week0v2/Engine/Source/Editor/PropertyEditor/ControlEditorPanel.cpp
+++ Week0v2/Engine/Source/Editor/PropertyEditor/ControlEditorPanel.cpp
@@ -267,7 +267,7 @@ void ControlEditorPanel::CreateModifyButton(ImVec2 ButtonSize, ImFont* IconFont)
         ImGui::Text("Blur Strength");
         float BlurStrength = GEngine->testBlurStrength;
         ImGui::SetNextItemWidth(120.0f);
-        if (ImGui::DragFloat("##Blur Strength", &BlurStrength, 0.03f, 0.0f, 1.0f, "%.1f"))
+        if (ImGui::DragFloat("##Blur Strength", &BlurStrength, 0.02f, 0.0f, 5.0f, "%.1f"))
         {
             GEngine->testBlurStrength = BlurStrength;
         }
