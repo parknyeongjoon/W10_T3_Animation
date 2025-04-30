f3ff0fa98a361501356f6686727c62bb45e0a1fd
nyeong <idpasudwns@gmail.com>
2025-04-30 16:09:56 +0900

    feat: 생성자 일부 수정

--- Week0v2/Engine/Source/Runtime/Engine/Camera/CameraDefine.h
+++ Week0v2/Engine/Source/Runtime/Engine/Camera/CameraDefine.h
@@ -8,23 +8,26 @@ class UCameraComponent;
 
 struct FViewInfo
 {
-    FVector Location;
-    FRotator Rotation;
-    float FOV;
+    FVector Location = FVector();
+    FRotator Rotation = FRotator();
+    float FOV = 0.0;
+
+    FViewInfo();
+    FViewInfo(const FVector InLocation, const FRotator& InRotation, const float InFOV) : Location(InLocation), Rotation(InRotation), FOV(InFOV) {}
 };
 
 struct FTViewTarget
 {
-    AActor* Target;
+    AActor* Target = nullptr;
     FViewInfo ViewInfo = FViewInfo();
 
-    FTViewTarget() : Target(nullptr) {}
+    FTViewTarget();
 };
 
 struct FPostProcessSettings
 {
     uint8 bOverride_ColorGamma = false;
-    FVector4 ColorGamma = FVector4(1,1,1,1);
+    FVector4 ColorGamma = FVector4();
 
-    FPostProcessSettings() {}
+    FPostProcessSettings();
 };
