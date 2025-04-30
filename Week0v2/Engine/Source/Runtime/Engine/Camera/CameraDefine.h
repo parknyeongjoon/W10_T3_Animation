a4bff33fa0749257d45c8e4cb8d833ac41ca366f
nyeong <idpasudwns@gmail.com>
2025-04-30 16:05:17 +0900

    feat: CameraModifer, FPlayerCameraManager 구조 설계

---
+++ Week0v2/Engine/Source/Runtime/Engine/Camera/CameraDefine.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Math/Rotator.h"
+#include "Math/Vector.h"
+#include "Math/Vector4.h"
+
+class AActor;
+class UCameraComponent;
+
+struct FViewInfo
+{
+    FVector Location;
+    FRotator Rotation;
+    float FOV;
+};
+
+struct FTViewTarget
+{
+    AActor* Target;
+    FViewInfo ViewInfo = FViewInfo();
+
+    FTViewTarget() : Target(nullptr) {}
+};
+
+struct FPostProcessSettings
+{
+    uint8 bOverride_ColorGamma = false;
+    FVector4 ColorGamma = FVector4(1,1,1,1);
+
+    FPostProcessSettings() {}
+};
---
+++ Week0v2/Engine/Source/Runtime/Engine/FPlayerCameraManager.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "Camera/CameraDefine.h"
+#include "Container/Array.h"
+
+class UCameraModifier;
+enum EViewTargetBlendOrder : int
+{
+    VTBlendOrder_Base,
+    VTBlendOrder_Override
+};
+
+class FPlayerCameraManager
+{
+public:
+    AActor* GetViewTarget() const { return ViewTarget.Target; }
+    FViewInfo& GetViewInfo() { return ViewTarget.ViewInfo; }
+    void AssignViewTarget(const FTViewTarget& InViewTarget) { ViewTarget = InViewTarget; }
+    
+    void AddCameraModifier(UCameraModifier* Modifier) { CameraModifiers.Add(Modifier); }
+    void RemoveCameraModifier(UCameraModifier* Modifier) { CameraModifiers.Remove(Modifier); }
+    void CleanCameraModifiers() { CameraModifiers.Empty(); }
+    void ApplyCameraModifiers(float DeltaTime, FViewInfo& ViewInfo);
+
+    /** Adds a postprocess effect at the given weight. */
+    void AddCachedPPBlend(FPostProcessSettings& PPSettings, float BlendWeight, EViewTargetBlendOrder BlendOrder = VTBlendOrder_Base);
+    void ClearCachedPPBlends();
+    /** Returns active post process info. */
+    void GetCachedPostProcessBlends(TArray<FPostProcessSettings> const*& OutPPSettings, TArray<float> const*& OutBlendWeights) const;
+    void GetCachedPostProcessBlends(TArray<FPostProcessSettings> const*& OutPPSettings, TArray<float> const*& OutBlendWeights, TArray<EViewTargetBlendOrder> const*& OutBlendOrders) const;
+private:
+    FTViewTarget ViewTarget = FTViewTarget();
+    TArray<UCameraModifier*> CameraModifiers;
+    
+    TArray<FPostProcessSettings> PostProcessBlendCache;
+    TArray<float> PostProcessBlendCacheWeights;
+    TArray<EViewTargetBlendOrder> PostProcessBlendCacheOrders;
+};
