8b0e348b70bbcdfcf5a0237cfdb4d92f0bcbd2c3
nyeong <idpasudwns@gmail.com>
2025-05-11 04:03:26 +0900

    feat: AnimDataModel 추가

---
+++ Week0v2/Engine/Source/Runtime/Core/Math/Transform.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Quat.h"
+#include "Vector.h"
+#include "UObject/ObjectMacros.h"
+
+class FTransform
+{
+public:
+    FTransform() : Rotation(FQuat::Identity), Location(FVector::ZeroVector), Scale(FVector::OneVector) {}
+    FTransform(FQuat InRotation, FVector InLocation, FVector InScale) : Rotation(InRotation), Location(InLocation), Scale(InScale) {}
+    
+    PROPERTY(FQuat, Rotation);
+    PROPERTY(FVector, Location);
+    PROPERTY(FVector, Scale);
+
+    const static FTransform Identity;
+    
+private:
+    FQuat Rotation;
+    FVector Location;
+    FVector Scale;
+};
+
+inline const FTransform FTransform::Identity = FTransform();
