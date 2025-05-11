038ab4b4e36ab64f5076ccc820113630254f401a
nyeong <idpasudwns@gmail.com>
2025-05-11 17:14:44 +0900

    chore: FBXLoader 접근 제한자 변경

--- Week0v2/Engine/Source/TestFBXLoader.h
+++ Week0v2/Engine/Source/TestFBXLoader.h
@@ -13,6 +13,20 @@ public:
     static bool InitFBXManager();
     static FSkeletalMeshRenderData* ParseFBX(const FString& FilePath);
 
+    static USkeletalMesh* CreateSkeletalMesh(const FString& FilePath);
+    static USkeletalMesh* GetSkeletalMesh(const FString& FilePath);
+    static const TMap<FString, USkeletalMesh*>& GetSkeletalMeshes() { return SkeletalMeshMap;}
+    
+    static FSkeletalMeshRenderData* GetSkeletalRenderData(FString FilePath);
+    static FSkeletalMeshRenderData GetCopiedSkeletalRenderData(FString FilePath);
+    static TMap<FName, FSkeletalMeshRenderData*> GetAllSkeletalMeshData() { return SkeletalMeshData; }
+    
+    static FRefSkeletal* GetRefSkeletal(FString FilePath);
+    static TMap<FName, FRefSkeletal*> GetAllRefSkeletal() { return RefSkeletalData; }
+    
+    static void UpdateBoundingBox(FSkeletalMeshRenderData& MeshData);
+private:
+    // Skeletal
     static void ExtractFBXMeshData(const FbxScene* Scene, FSkeletalMeshRenderData* MeshData, FRefSkeletal* RefSkeletal);
     static void ExtractBoneFromNode(FbxNode* Node, FSkeletalMeshRenderData* MeshData, FRefSkeletal* RefSkeletal);
     static void ExtractMeshFromNode(FbxNode* Node, FSkeletalMeshRenderData* MeshData, FRefSkeletal* RefSkeletal);
@@ -26,30 +40,16 @@ public:
     static void ProcessSkinning(FbxSkin* skin, FSkeletalMeshRenderData* mesh_data, FRefSkeletal* RefSkeletal, int base_vertex_index);
     static void ExtractIndices(FbxMesh* Mesh, FSkeletalMeshRenderData* MeshData, int BaseVertexIndex);
     static void ExtractMaterials(FbxNode* Node, FbxMesh* Mesh, FSkeletalMeshRenderData* MeshData, FRefSkeletal* RefSkeletal, int BaseIndexOffset);
-    static void UpdateBoundingBox(FSkeletalMeshRenderData* MeshData);
+    
+    static FObjMaterialInfo ConvertFbxToObjMaterialInfo(FbxSurfaceMaterial* FbxMat, const FString& BasePath = TEXT(""));
+    static FSkeletalVertex& GetVertexFromControlPoint(FbxMesh* Mesh, int PolygonIndex, int VertexIndex);
+    static bool IsTriangulated(FbxMesh* Mesh);
 
+    
+    // Anim
     static void ExtractFBXAnimData(const FbxScene* scene, TArray<FSkeletalAnimation>& AnimData);
     static void ExtractAnimClip(FbxAnimStack* AnimStack, FSkeletalAnimation& AnimClip, const TArray<FbxNode*>& BoneNodes);
     static void ExtractAnimTrack(FbxAnimLayer* AnimLayer, FbxNode* BoneNode, FRawAnimSequenceTrack& AnimTrack);
-
-    static FSkeletalMeshRenderData* GetSkeletalRenderData(FString FilePath);
-    static FSkeletalMeshRenderData GetCopiedSkeletalRenderData(FString FilePath);
-    
-    static FRefSkeletal* GetRefSkeletal(FString FilePath);
-    static TMap<FName, FRefSkeletal*> GetAllRefSkeletals() { return RefSkeletalData; }
-    
-    static void UpdateBoundingBox(FSkeletalMeshRenderData MeshData);
-    
-    static FObjMaterialInfo ConvertFbxToObjMaterialInfo(FbxSurfaceMaterial* FbxMat, const FString& BasePath = TEXT(""));
-
-    static USkeletalMesh* CreateSkeletalMesh(const FString& FilePath);
-    static TMap<FName, FSkeletalMeshRenderData*> GetAllSkeletalMeshes() { return SkeletalMeshData; }
-    static USkeletalMesh* GetSkeletalMesh(const FString& FilePath);
-    static USkeletalMesh* GetSkeletalMeshData(FString FilePath) { return SkeletalMeshMap[FilePath]; }
-    static const TMap<FString, USkeletalMesh*>& GetSkeletalMeshes() { return SkeletalMeshMap;}
-
-    static FSkeletalVertex& GetVertexFromControlPoint(FbxMesh* Mesh, int PolygonIndex, int VertexIndex);
-    static bool IsTriangulated(FbxMesh* Mesh);
 
 private:
     inline static FbxManager* FbxManager;
