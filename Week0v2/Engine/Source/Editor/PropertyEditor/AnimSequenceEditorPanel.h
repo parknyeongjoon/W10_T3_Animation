851b1466393406441840f556bb8fba7135b56d61
Kimu-Chan <woochankim0303@gmail.com>
2025-05-14 01:20:19 +0900

    wip: AnimSequence 및 Notify Track 레이아웃 초안

---
+++ Week0v2/Engine/Source/Editor/PropertyEditor/AnimSequenceEditorPanel.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Define.h"
+#include "UnrealEd/EditorPanel.h"
+
+class ULevel;
+class UActorComponent;
+class USceneComponent;
+class USkeletalMeshComponent;
+class UAnimDataModel;
+
+class AnimSequenceEditorPanel : public UEditorPanel
+{
+public:
+    void Initialize(float InWidth, float InHeight);
+    virtual void Render() override;
+    virtual void OnResize(HWND hWnd) override;
+
+private:
+    // @todo Notify Color
+    //void RGBToHSV(float r, float g, float b, float& h, float& s, float& v) const;
+    //void HSVToRGB(float h, float s, float v, float& r, float& g, float& b) const;
+
+private:
+    float Width = 0, Height = 0;
+
+    USkeletalMeshComponent* SkeletalMeshComponent = nullptr;
+    UAnimDataModel* PrevAnimDataModel = nullptr;
+
+    int32 PreviousFrame = 0;
+    int32 SelectedTrackIndex_ForRename = INDEX_NONE;
+    int32 SelectedNotifyGlobalIndex_ForRename = INDEX_NONE;
+    TCHAR RenameTrackBuffer[256];
+    TCHAR RenameNotifyBuffer[256];
+
+    char NewNotifyNameBuffer[128] = "NewNotify";
+    float NewNotifyTime = 0.0f;
+    float RenameNotifyDuration = 1.0f;
+};
